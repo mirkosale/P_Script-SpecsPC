2edbb37c78373d63e89ea023274ee90eb66bde01
Mirko Sale <pb51vab@etmlnet.local>
2022-05-18 08:55:18 +0200

    Fixed remoting by creating same accounts on both test machines and by removing check - adding back later

--- TimMirVep-Get-RemotePCSpecs.ps1
+++ TimMirVep-Get-RemotePCSpecs.ps1
@@ -15,30 +15,30 @@
  	*****************************************************************************
 
 .SYNOPSIS
-    Obtenir les spÃ©cifications d'une liste de PC Ã  distance
+    Obtenir les spécifications d'une liste de PC à distance
  	
 .DESCRIPTION
-    Ce script une fois exÃ©cutÃ© va rechercher dans toute la liste de PC passÃ©e via 
-    les arguments et va rÃ©cupÃ©rer les informations sur le PC (telles que le 
-    processeur, le nom de la machine, etc.) existants dans le rÃ©seau en vÃ©rifiant
-    qu'ils existent via le nom . Ces informations vont ensuite Ãªtre rÃ©cupÃ©rÃ©es 
-    et stockÃ©es dans un fichier texte et organisÃ©es sous forme de tableau. Ces
-    informations vont ensuite Ãªtre placÃ©es dans un dossier de logs avec un nom
+    Ce script une fois exécuté va rechercher dans toute la liste de PC passée via 
+    les arguments et va récupérer les informations sur le PC (telles que le 
+    processeur, le nom de la machine, etc.) existants dans le réseau en vérifiant
+    qu'ils existent via le nom . Ces informations vont ensuite être récupérées 
+    et stockées dans un fichier texte et organisées sous forme de tableau. Ces
+    informations vont ensuite être placées dans un dossier de logs avec un nom
     de fichier unique comme nom de fichier texte.
     
 
 .OUTPUTS
-    Un fichier avec les diffÃ©rentes informations des PCs et un fichier
-    qui va rÃ©pertorier les erreurs qui se sont produites.
+    Un fichier avec les différentes informations des PCs et un fichier
+    qui va répertorier les erreurs qui se sont produites.
 	
 .EXAMPLE
     ./TimMirVep-Get-RemotePCSpecs.ps1 Win10-C1 Win10-C2
     Return dans un fichier logs se trouvant dans un dossier logs :
-    DATE | Carte mÃ¨re | Processeur | Carte graphique - VRAM | RAM | QuantitÃ© - Type | Disque dur | Espace des PC 
+    DATE | Carte mère | Processeur | Carte graphique - VRAM | RAM | Quantité - Type | Disque dur | Espace des PC 
     Exemple : 20.04.2020 | ASRock Z390 Taichi Ultimate | I9-9900K - 3.6GHz | RTX2080TI - 11GB | Corsair - 32GB - DDR4 | SSD Samsung EVO 860 - 500Gb
 #>
 
-#Si l'utilisateur n'a pas entrÃ© d'arguments
+#Si l'utilisateur n'a pas entré d'arguments
 if (!$args)
 {
     Get-Help $MyInvocation.MyCommand.Path
@@ -47,11 +47,12 @@ if (!$args)
 else
 {
     $errors=@()
+    $logs=@()
 
     #Check for administrator rights
     if ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544')
     {
-        #Reprend la date lors de l'exÃ©cution du script
+        #Reprend la date lors de l'exécution du script
         $date = Get-Date
         $dateSpec = $date.ToString("dd.MM.yyyy")
         $date = $date.ToString("yyyy-MM-dd-HH-mm-ss")
@@ -65,7 +66,7 @@ else
         New-Item -Path $logFolder -ItemType Directory -ErrorAction Ignore
         if (!$?)
         {
-            $errors += "Le dossier logs existe dÃ©jÃ  Ã  cet emplacement"
+            $errors += "Le dossier logs existe déjà à cet emplacement"
         }
 
         #Chemin du fichier
@@ -81,34 +82,36 @@ else
             }
             catch
             {
-                $errors += "La session n'a pas pu Ãªtre crÃ©Ã©e sur le PC $PC car les identifiants sont incorrects ou la machine n'existe pas."
+                $errors += "La session n'a pas pu être créée sur le PC $PC car les identifiants sont incorrects ou la machine n'existe pas."
             }
 
             Invoke-Command -Session $remotingSession -ScriptBlock{(Get-WmiObject Win32_ComputerSystem).Name | Write-Output >> $filePath}
-            Write-Host "DATE |Carte mÃ¨re | Processeur | Carte graphique â€“ VRAM | RAM â€“ QuantitÃ© â€“ Type | Disque dur â€“ Espace"
-            Write-Host $dateSpec
+            $logs += "DATE | Carte mère | Processeur | Carte graphique - VRAM | RAM - Quantité - Type | Disque dur - Espace"
             
 
-            #RÃ©cupÃ©ration des informations si la session a pu Ãªtre Ã©tablie
-            if ($remotingSession -eq -not $null)
-            {
-                $diskModel = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-WmiObject Win32_DiskDrive | where {$_.DeviceID -eq "\\.\PHYSICALDRIVE0"}).Model}
-                #
-                #https://www.improvescripting.com/how-to-get-disk-size-and-disk-free-space-using-powershell
-                #https://ardamis.com/2012/08/21/getting-a-list-of-logical-and-physical-drives-from-the-command-line/
-                $diskGb = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-WmiObject Win32_DiskDrive | where {$_.DeviceID -eq "\\.\PHYSICALDRIVE0"}).Size/1gb}
+            #Récupération des informations si la session a pu être établie
+          
 
-                #
-                #https://devblogs.microsoft.com/scripting/powertip-use-powershell-to-round-to-specific-decimal-place/
-                $diskGb = [string]([math]::Round($diskGb, 0)) + "Gb" 
+               
 
                 $moboManufacturer = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-WmiObject Win32_BaseBoard).Manufacturer}
                 $moboProduct = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-WmiObject Win32_BaseBoard).Product}
                 $cpuName = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-CimInstance CIM_Processor).Name}
                 $cpuClock = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-CimInstance CIM_Processor).MaxClockSpeed}
 
+                $diskModel = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-WmiObject Win32_DiskDrive | where {$_.DeviceID -eq "\\.\PHYSICALDRIVE0"}).Model}
+                #
+                #https://www.improvescripting.com/how-to-get-disk-size-and-disk-free-space-using-powershell
+                #https://ardamis.com/2012/08/21/getting-a-list-of-logical-and-physical-drives-from-the-command-line/
+                $diskGb = Invoke-Command -Session $remotingSession -ScriptBlock{(Get-WmiObject Win32_DiskDrive | where {$_.DeviceID -eq "\\.\PHYSICALDRIVE0"}).Size/1gb}
+                #
+                #https://devblogs.microsoft.com/scripting/powertip-use-powershell-to-round-to-specific-decimal-place/
+                $diskGb = [string]([math]::Round($diskGb, 0)) + "Gb" 
                 
-            }
+                $logs += "$PC : $dateSpec | $moboManufacturer $moboProduct | $cpuName - $cpuClock | $gpuName - $gpuVRAM | $ramName - $ramAmount - $ramType | $diskModel - $diskGB"
+            # if ($remotingSession -eq $null)
+            # {
+            #     echo a  }
             else
             {
                 $errors += "Erreur session"
@@ -118,7 +121,10 @@ else
         foreach ($error in $errors) {
             Write-Output "$error" >> $errorPath
         }
-
+        
+        foreach ($log in $logs) {
+            Write-Output "$log" >> $logPath
+        }
     } #endif Admin rights check
     else
     {
